8fb8703610b2c22125b688d8db658ee2b6360f65
mskian <veerfun23@gmail.com>
2022-08-02 16:49:25 +0530

    add api cache and rate limit

--- index.js
+++ index.js
@@ -1,56 +1,100 @@
-const express = require('express');
-const moment = require('moment');
-const sslChecker = require('ssl-checker');
+const express = require("express");
+const moment = require("moment");
+const sslChecker = require("ssl-checker");
+const apicache = require("apicache");
+const { rateLimit } = require("express-rate-limit");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
+const cache = apicache.middleware;
 
-app.get('/:domain', function(req, res) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('X-Frame-Options', 'DENY');
-    res.header('X-XSS-Protection', '1; mode=block');
-    res.header('X-Content-Type-Options', 'nosniff');
-    res.header('Strict-Transport-Security', 'max-age=63072000');
-    res.setHeader('Content-Type', 'application/json');
-    app.disable('x-powered-by');
-
-    const get_domain = req.params.domain;
-    sslChecker(get_domain).then((certdata) => {
-
-        var startdate = new Date(certdata.validFrom);
-        var enddate = new Date(certdata.validTo);
-        var certstart = moment(startdate);
-        var certend = moment(enddate);
-        var ssldata = ([{
-            domain: get_domain,
-            issued: certstart.format('LLLL'),
-            expires: certend.format('LLLL'),
-            daysleft: certdata.daysRemaining
-        }]);
-        res.send(JSON.stringify(ssldata, null, 4));
-        console.log(JSON.stringify(ssldata, null, 4));
-
-    }).catch((err) => {
-        if (err.code === 'ENOTFOUND') {
-            console.log('Fix Hostname or Provide Correct Domain Name');
-            res.send(JSON.stringify('false'));
-        } else if (err.code === 'ECONNREFUSED') {
-            console.log('Fix Hostname or Provide Correct Domain Name');
-            res.send(JSON.stringify('false'));
-        }
-    });
-
+const apiRequestLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 40,
+  handler: function (req, res) {
+    return res.status(429).json([
+      {
+        domain: "Not Found",
+        issued: "Not Found",
+        expires: "Not Found",
+        daysleft: "Not Found",
+      },
+    ]);
+  },
 });
 
-app.use('/', function(req, res) {
-    res.status(404).json({
-        error: 1,
-        message: 'Enter a valid Domain URL'
+app.get("/:domain", cache("1 hour"), apiRequestLimiter, function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With"
+  );
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("X-Frame-Options", "DENY");
+  res.header("X-XSS-Protection", "1; mode=block");
+  res.header("X-Content-Type-Options", "nosniff");
+  res.header("Strict-Transport-Security", "max-age=63072000");
+  res.setHeader("Content-Type", "application/json");
+  app.disable("x-powered-by");
+
+  const get_domain = req.params.domain;
+  sslChecker(get_domain)
+    .then((certdata) => {
+      var startdate = new Date(certdata.validFrom);
+      var enddate = new Date(certdata.validTo);
+      var certstart = moment(startdate);
+      var certend = moment(enddate);
+      var ssldata = [
+        {
+          domain: get_domain,
+          issued: certstart.format("LLLL"),
+          expires: certend.format("LLLL"),
+          daysleft: certdata.daysRemaining,
+        },
+      ];
+      res.send(JSON.stringify(ssldata, null, 4));
+      console.log(JSON.stringify(ssldata, null, 4));
+    })
+    .catch((err) => {
+      if (err.code === "ENOTFOUND") {
+        console.log("Fix Hostname or Provide Correct Domain Name");
+        res.json([
+          {
+            domain: "Not Found",
+            issued: "Not Found",
+            expires: "Not Found",
+            daysleft: "Not Found",
+          },
+        ]);
+      } else if (err.code === "ECONNREFUSED") {
+        console.log("Fix Hostname or Provide Correct Domain Name");
+        res.json([
+          {
+            domain: "Not Found",
+            issued: "Not Found",
+            expires: "Not Found",
+            daysleft: "Not Found",
+          },
+        ]);
+      }
     });
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+app.use("/", function (req, res) {
+  res.status(404).json({
+    error: 1,
+    message: "Page or Data not Found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  if (!err) return next();
+  return res.status(403).json({
+    error: 1,
+    message: "Page or Data not Found",
+  });
+});
+
+app.listen(port, function () {
+  console.log("listening on port " + port);
 });
